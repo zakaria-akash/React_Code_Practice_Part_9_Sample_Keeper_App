84c7658ea8dd63ad0ebfe65520b7ea75acec1498
Zakaria Ibrahim <59124067+zakaria-akash@users.noreply.github.com>
2022-05-03 05:19:36 +0600

    applying of JavaScript arrow function in Note.jsx file

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,14 +2,30 @@ import React from "react";
 import EntryCard from "./EntryCard";
 import Notes from "../notes.js";
 
-function InitEntry(Notes) {
-  return (
-    <EntryCard key={Notes.key} title={Notes.title} content={Notes.content} />
-  );
-}
+// function InitEntry(noteItem) {
+//   return (
+//     <EntryCard
+//       key={noteItem.key}
+//       title={noteItem.title}
+//       content={noteItem.content}
+//     />
+//   );
+// }
+
+// function Note() {
+//   return <div> {Notes.map(InitEntry)} </div>;
+// }
+
+//applying of javascript arrow function:
 
 function Note() {
-  return <div> {Notes.map(InitEntry)} </div>;
+  return (
+    <div>
+      {Notes.map((item) => (
+        <EntryCard key={item.key} title={item.title} content={item.content} />
+      ))}
+    </div>
+  );
 }
 
 export default Note;
