567438f965facbda5427fb768f8093dfb833eca4
Zakaria Ibrahim <59124067+zakaria-akash@users.noreply.github.com>
2022-05-12 01:41:30 +0600

    changing the title and some css properties

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -20,7 +20,7 @@ import Notes from "../notes.js";
 
 function Note() {
   return (
-    <div>
+    <div className="entireNote">
       {Notes.map((item) => (
         <EntryCard key={item.key} title={item.title} content={item.content} />
       ))}
