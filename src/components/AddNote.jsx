756246d3e133fc50c10892646257cf5ee45f9544
Zakaria Ibrahim <59124067+zakaria-akash@users.noreply.github.com>
2022-05-13 04:40:54 +0600

    enable adding a new note functionality

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,13 +1,52 @@
-import React from "react";
-
+import React, { useState } from "react";
+import EntryCard from "./EntryCard";
 function AddNote() {
+  const [newNote, setNewNote] = useState([]);
+  let newTitle;
+  let newContent;
+  let newObject = {
+    title: "",
+    content: ""
+  };
+  function collectNewTitle(e) {
+    newTitle = e.target.value;
+  }
+  function collectNewContent(e) {
+    newContent = e.target.value;
+  }
+  function createANote(ev) {
+    newObject.title = newTitle;
+    newObject.content = newContent;
+    setNewNote((oldItems) => {
+      return [...oldItems, newObject];
+    });
+    ev.preventDefault();
+  }
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+      <form onSubmit={createANote}>
+        <input
+          onChange={collectNewTitle}
+          name="title"
+          placeholder="Title"
+          aria-required="true"
+          value={newTitle}
+        />
+        <textarea
+          onChange={collectNewContent}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          aria-required="true"
+          value={newContent}
+        />
         <button>Add Note</button>
       </form>
+      <div>
+        {newNote.map((item, index) => (
+          <EntryCard key={item.key} title={item.title} content={item.content} />
+        ))}
+      </div>
     </div>
   );
 }
